8267343a39ec684a65fc4d97648a0cd430122bef
j_4k <jakubiakrjulian@gmail.com>
2023-12-10 18:59:11 +0100

    added 2 done

--- zadanie2.m
+++ zadanie2.m
@@ -11,7 +11,7 @@ y2=x2;
 
 fun=(sin(2*x1)+0.2.*sin(8*x1)).*exp(-x1)*5;
 
-
+%%%
 range=[-20 20];
 liczba_n_h1=30;
 liczba_n_h2=10;
@@ -24,15 +24,24 @@ siec.trainParam.goal=0;
 siec=train(siec,x,fun);
 ynn=sim(siec,x);
 
+%%%
 x2seq=con2seq(x2);
 sseq=con2seq(fun);
-
 nn_elm_model=newelm(x2,fun,10);
 nn_elm_model = train(nn_elm_model,x2seq,sseq);
 
 ynn1=nn_elm_model(x2seq);
 ynn1=cell2mat(ynn1);
+%%%
 
+MN=10;
+DF=3;
+GOAL=0;
+SPREAD=0.5;
+NN_model_rbf=newrb(x2,fun,GOAL,SPREAD,MN,DF);
+NN_model_rbfe=newrbe(x2,fun,SPREAD);
+ynn2=sim(NN_model_rbf,x2);
+ynn3=sim(NN_model_rbfe,x2);
 
 
 figure(1)
@@ -53,3 +62,19 @@ plot(fun,'b');
 legend('matching', 'input')
 mseValue2 = mse(fun,ynn1);
 title(['2, siec rekurencyjna, newlm, MSE = ', num2str(mseValue2)]);
+
+
+figure(3)
+plot(ynn2, 'r-');
+grid;
+hold on;
+title('1', 'newrb vs newrbe');
+plot(ynn3, 'g-')
+plot(fun, 'b--');
+legend('rbf', 'rbfe', 'function');
+e1=mse(fun, ynn2);
+e2=mse(fun, ynn3);
+
+
+text(1, 1, ['e1 = ', num2str(e1)]);
+text(1, 0.9, ['e2 = ', num2str(e2)]);
